b8a1978777b63bcf0990eb4e9fdd4adb4f7f4484
MaroiuMihail <mihai.maroiu88@gmail.com>
2024-12-02 18:04:01 +0200

    prima versiune a proiectului

---
+++ src/test/java/helpMethods/ElementMethods.java
@@ -0,0 +1,14 @@
+package helpMethods;
+
+import org.openqa.selenium.WebDriver;
+
+public class ElementMethods {
+
+    public WebDriver driver;
+
+    public ElementMethods(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+}
