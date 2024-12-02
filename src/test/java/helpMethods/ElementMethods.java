d94b6ad3cdae163d196ec8e81487e4851398dff5
MaroiuMihail <mihai.maroiu88@gmail.com>
2024-12-02 19:41:32 +0200

    Am terminat de refactorizat primul test

--- src/test/java/helpMethods/ElementMethods.java
+++ src/test/java/helpMethods/ElementMethods.java
@@ -1,6 +1,13 @@
 package helpMethods;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class ElementMethods {
 
@@ -10,5 +17,27 @@ public class ElementMethods {
         this.driver = driver;
     }
 
+    public void clickElement(WebElement element){
+        waitVisibleElement(element);
+        element.click();
 
+    }
+
+    public void clickJSElement(WebElement element){
+        waitVisibleElement(element);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", element);
+    }
+
+    public void fillElement(WebElement element, String text){
+        waitVisibleElement(element);
+        element.sendKeys(text);
+
+    }
+
+    public void waitVisibleElement(WebElement element){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(element));
+
+    }
 }
--- src/test/java/tests/TabWindowTest.java
+++ src/test/java/tests/TabWindowTest.java
@@ -1,5 +1,7 @@
 package tests;
 
+import helpMethods.ElementMethods;
+import helpMethods.TabMethods;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -23,41 +25,28 @@ public class TabWindowTest {
         driver.get("https://demoqa.com/");
         driver.manage().window().maximize();
 
+        ElementMethods elementMethods = new ElementMethods(driver);
+        TabMethods tabMethods = new TabMethods(driver);
+
+
         WebElement alertframewindowMenu = driver.findElement(By.xpath("//h5[text()='Alerts, Frame & Windows']"));
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].click();", alertframewindowMenu);
+        elementMethods.clickJSElement(alertframewindowMenu);
 
         WebElement browserwindowMenu= driver.findElement(By.xpath("//span[text()='Browser Windows']"));
-        js.executeScript("arguments[0].click();", browserwindowMenu);
+        elementMethods.clickJSElement(browserwindowMenu);
 
         WebElement newTabElement = driver.findElement(By.id("tabButton"));
-        js.executeScript("arguments[0].click();",newTabElement);
+        elementMethods.clickJSElement(newTabElement);
 
-        System.out.println("URL-ul paginii curente este"+ driver.getCurrentUrl());
-
-        List<String> tabsList = new ArrayList<>(driver.getWindowHandles());
-        driver.switchTo().window(tabsList.get(1));
-
-        System.out.println("URL-ul paginii curente este"+ driver.getCurrentUrl());
-
-        driver.close();
-
-        driver.switchTo().window(tabsList.get(0));
-
-        System.out.println("URL-ul paginii curente este"+ driver.getCurrentUrl());
+        tabMethods.switchSpecificTab(1);
+        tabMethods.closeCurrentTab();
+        tabMethods.switchSpecificTab(0);
 
         WebElement newWindowElement = driver.findElement(By.id("windowButton"));
-        js.executeScript("arguments[0].click()",newWindowElement);
+        elementMethods.clickJSElement(newWindowElement);
 
-        System.out.println("URL-ul paginii curente este "+ driver.getCurrentUrl());
-
-        List<String> windowList = new ArrayList<>(driver.getWindowHandles());
-        driver.switchTo().window(windowList.get(1));
-
-        System.out.println("URL-ul paginii curente este "+ driver.getCurrentUrl());
-
-        driver.close();
-
+        tabMethods.switchSpecificTab(1);
+        tabMethods.closeCurrentTab();
         driver.quit();
 
 
